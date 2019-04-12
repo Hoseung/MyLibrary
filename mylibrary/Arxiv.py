4da410f2f5e3822a111dfa0d734e48ef21db6e5f
Hoseung Choi <hopung@gmail.com>
2019-04-12 16:03:14 +0200

    Arxiv meta as dict, incomplete - arxiv custom format is too complex...!

--- mylibrary/Arxiv.py
+++ mylibrary/Arxiv.py
@@ -22,9 +22,14 @@ Check if published version is available.
 """
 
 from urllib3 import PoolManager
+"""
+Will it be better to keep PoolManager as a single, global instance?
+"""
+http = PoolManager()
+
 
 def get_sentence_standard(ll, tag):
-    return ll.split("</{}>".format(tag))[-2].split("<{}>".format(tag))[-1]
+    return ll.strip("<"+tag).split(">")[1].split("<")[0]
 
 def parse_arxiv(arx, qresult):
     """
@@ -39,43 +44,45 @@ def parse_arxiv(arx, qresult):
     """
 
     lines = qresult.decode("UTF-8").split("\n")
+    key=None
     entry=False
-    _ibeg_summary=None
-    _iend_summary=None
-    read_author=False
+
+    i_beg=-1
+    i_end=-1
+
     for i, ll in enumerate(lines):
         if "</entry>" in ll:
             break
         if "<entry>" in ll:
             entry=True
+            continue
+        
         if entry:
-            if arx.id is None and "<id>" in ll:
-                arx.id=get_sentence_standard(ll, "id").split("/")[-1]
-            if arx.updated is None and "<updated>" in ll:
-                arx.updated = get_sentence_standard(ll, "updated")
-            if arx.published is None and "<published>" in ll:
-                arx.published = get_sentence_standard(ll, "published")
-            if arx.title is None and "<title>" in ll:
-                arx.title = get_sentence_standard(ll, "title")
-            if _ibeg_summary is None and "<summary>" in ll:
-                _ibeg_summary = i
-            if _iend_summary is None and "</summary>" in ll:
-                _iend_summary = i
-            if read_author:
-                arx.authors.append(get_sentence_standard(ll, "name"))
-                read_author=False
-            if not read_author and "<author>" in ll:
-                read_author = True
-            if arx.doi is None and "":
-                arx.doi = ll.split(">")[1].split("<")[0]
-            if arx.primary_category is None and "arxiv:primary_category" in ll:
-                arx.primary_category = ll.split('"')[3]
-            if "category term" in ll:
-                arx.categories.append(ll.split('"')[1])
-                
-
-    arx.summary = "".join(lines[_ibeg_summary:_iend_summary]).split("<summary>")[1]
-    
+            if i_beg <0:
+                # update key
+                try:
+                    key = ll.split("<")[1].split(">")[0]
+                except:
+                    continue
+                try:
+                    key = key.split()[0].split(":")[1]
+                    key_escape = "arxiv:" + key
+                except:
+                    key_escape = key
+                    pass
+                if key in arx.meta.keys():
+                    #if arx.meta[key] is None:
+                    i_beg=i
+                        
+                    if "</{}>".format(key_escape) in ll:
+                        # If one-line string
+                        i_end=i
+            elif "</{}>".format(key_escape) in ll:
+                # if multi-line string
+                i_end = i            
+            if i_end > 0:
+                arx.meta[key]=get_sentence_standard("".join(lines[i_beg:i+1]).strip(), key_escape)#.split("/")[-1]
+                i_beg=i_end=-1
 
 
 #def 
@@ -84,24 +91,28 @@ class Arxiv_meta():
     """
     """
     def __init__(self, id=None, load=True):
-        self.title=None
-        self.published=None
-        self.updated=None
-        self.authors=[]
-        self.primary_category=None
-        self.categories=[]
-        self.summary=None
-        self.doi=None
-        self.id=str(id)
+        self.meta = dict(
+            title=None,
+            published=None,
+            updated=None, 
+            authors=None,
+            primary_category=None,
+            categories=[],
+            summary=None, 
+            doi=None,
+            id=str(id),
+            journal_ref=None,
+            )
+        
         if load:
-            self.query_by_id(self.id)
+            self.query_by_id(self.meta["id"])
+
+    def _get_ads_query(self):
+        return http.request("GET", "http://export.arxiv.org/api/query?id_list={}".format(self.meta["id"]))
 
     def query_by_id(self, parse=True, return_raw=False):
-        """
-        Will it be better to keep PoolManager as a single, global instance?
-        """
-        http = PoolManager()
-        r = http.request("GET", "http://export.arxiv.org/api/query?id_list={}".format(self.id))
+        r = self._get_ads_query()
+        
         if parse:
             parse_arxiv(self, r.data)
         if return_raw:
